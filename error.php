9905d5660fcb0917a041dc7cb33e4d10ad35e9bb
Theresa Calangian <theresacalangian@Theresas-MacBook-Pro.local>
2021-05-19 16:38:17 +0800

    Error page

---
+++ error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <div>
+        <img src="logo1.png" alt="Paris" class="center"> 
+        <h1 class="login-title" style="color:white;">LRC Online</h1>
+        <p class="upm" style="color:white;">University of the Philippines Manila</p>
+    </div>
+
+    <div class='form'>
+        <h3 style="color:white;" > Incorrect Username/password.</h3><br/>
+        <p class='link'>Click here to <a href= 'login.php'>login</a> again.</p>
+    </div>
+
+</body>
+</html>
