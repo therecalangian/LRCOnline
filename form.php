cd05f957bef80d1519af815e6f27a4f3c1234a82
Althea Maramba <62100069+altheamaramba@users.noreply.github.com>
2021-06-05 17:56:26 +0800

    updated

--- form.php
+++ form.php
@@ -79,7 +79,7 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="index_student.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="about_student.php">About</a></li>
                     <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Schedule</a>
                       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
